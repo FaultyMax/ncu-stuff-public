a02ee6ed71e8d1947e81397b9054066c4b6aeaac
GuessWho <faultymax@gmail.com>
2025-11-09 00:49:19 +0100

    samochod

---
+++ cpp_lang/d_les/samochody.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+class vehicle{
+
+  public:
+  
+    string name;
+    float avg_speed;
+    float avg_consumpt;
+    float current_fuel;
+    float road_left;
+    int trunk_size;
+    int* trunk;
+    
+    vehicle(){
+      trunk_size = 5 + rand() % 16;
+      trunk = new int[trunk_size];
+      for(int i = 0 ; i < trunk_size ; i++){ trunk[i] = rand() % 11; cout << i << ": " << trunk[i] << endl;}
+    }
+
+    vehicle(string name){
+    
+      this->name = name;
+      avg_speed = 5 + rand() % 91;
+      avg_consumpt = avg_speed / 100f;
+      current_fuel = 5 + rand() % 46;
+      road_left = 25 + rand() % 200;
+      
+      trunk_size = 5 + rand() % 16;
+      trunk = new int[trunk_size];
+      for(int i = 0 ; i < trunk_size ; i++){ trunk[i] = rand() % 11; cout << i << ": " << trunk[i] << endl;}
+    }
+    
+    ~vehicle(){ delete[] trunk; }
+};
+
+int main(){
+
+  srand(time(0));
+  
+  vehicle my_new;
+  vehicle another_one("yoo");
+  
+  return 0;
+  
+}
+
+
+
+
+
+/*
+Utwórz klasę reprezentującą samochód (w trakcie podróży) zawierającą pola:
+
+nazwa - string
+średnia prędkość w km/h - float
+średnie spalanie / 100km - float
+aktualna ilość paliwa - float
+pozostały do przejechania dystans w km - float
+rozmiar bagażnika - int o losowej wartości z zakresu [5; 20], mówi o tym ile rzeczy zmieści się w bagażniku
+bagażnik reprezentowany przez dynamicznie zaalokowaną tablicę intów. Rozmiar tablicy jest równy rozmiarowi bagażnika z poprzedniego punktu zadania.
+Każde pole tej tablicy ustaw na losową wartość z zakresu [0; 10]. Każde pole oznacza wagę jednego przedmiotu w bagażniku. 0 oznacza brak przedmiotu w danym miejscu.
+
+Oraz metody, które mają zrealizować następujące zadania:
+
+Sprawdzić, czy na aktualnej ilości paliwa auto może dojechać do celu oraz zwrócić true jeśli tak, i false jeśli nie
+Zwrócić dystans (w kilometrach) jaki może pokonać auto na aktualnej ilości paliwa
+Zwrócić w jakim czasie auto dojedzie do celu. Jeśli nie jest to możliwe ze względu na brak paliwa zwróć -1
+Sprawdzić, czy w bagażniku jest przynajmniej n wolnych miejsc; n ma zostać przekazane jako parametr metody. Metoda zwraca true jeśli są wolne miejsca i false, jeśli nie.
+Obliczyć średnią wagę rzeczy z bagażnika (uwaga, pamiętaj, że wartość 0 oznacza brak przedmiotu w bagażniku!)
+Ponadto utwórz konstruktory:
+
+Przyjmujący parametr nazwa - pozostałe zmienne przydziel losowo (zadbaj o poprawność losowych danych, na przykład, żeby wartości nie były ujemne, albo prędkość była > 0).
+Bezargumentowy, działający tak jak powyższy, z tą różnicą, że nazwa samochodu ma być ustawiona na "Auto".
+Przyjmujący parametry nazwa, średnia prędkość, średnie spalanie, aktualna ilość paliwa, pozostały do przejechania dystans i rozmiar bagażnika.
+(jeśli bagażnik będzie miał rozmiar spoza zakresu [5; 20] ustaw go na najbliższą poprawną wartość [np. rozmiar = 3 zostanie ustawiony na 5, rozmiar = 1034 zostanie ustawiony na 20])
+Pamiętaj, żeby konstruktory utworzyły bagażnik!
+
+Zwalnianiem pamięci przeznaczonej na bagażnik ma zająć się destruktor.
+
+Nazwy pól i metod są dowolne. Proszę zadbać o to, żeby ich nazwy reprezentowały zadanie, które mają realizować oraz żeby dobrać poprawne typy zmiennych do realizacji zadań.
+
+--------------------
+
+Utwórz funkcję (ale nie metodę klasy!) która przyjmie za argumenty dwa wskaźniki na obiekty klasy samochód i zwróci nazwę samochodu, który jako pierwszy dojedzie do celu. W przypadku remisu zwróć słowo "---REMIS---".
+
+--------------------
+
+W funkcji main:
+
+Utwórz tablicę 20-tu wskaźników na samochód.
+15 z nich utwórz pierwszym konstruktorem, pozostałe drugim (operator new zwraca wskaźnik)
+Dla każdego z samochodów wypisz informacje czy dojedzie do celu
+Wybierz parę samochodów, które dojadą do celu i sprawdź, który dojedzie szybciej (dowolnie obsłuż brak takiej pary)
+Znajdź samochód o 3 pustych miejscach w bagażniku, jeśli nie ma takiego, wypisz stosowną informację, Jeśli jest, wypisz jego średnią wagę bagażu.
+Znajdź najszybszy samochód
+Znajdź samochód o najmniejszym zasięgu (taki, który może przejechać najkrótszy dystans)
+Wypisz samochody, które dojadą do celu w czasie dłuższym niż 1 godzina.
+Pamiętaj aby zwolnić pamięć (każdy samochód osobno!)
+Wskazówka:
+
+Pracuj na tablicy wskaźników na samochód. Praca z tablicą obiektów tak jak na zajęciach (Film filmy[100]) prawdopodobnie spowoduje, że program zakończy swoją pracę błędem. 
+*/
